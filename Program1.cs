568f2b1887867902f60286bb59652a7235ebcfb2
utkinatamara <166318629+utkinatamara@users.noreply.github.com>
2024-05-21 15:18:06 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,20 @@
+﻿//задача 2
+//Задайте произвольный массив. Выведете
+//его элементы, начиная с конца. Использовать
+//рекурсию, не использовать циклы.*/
+
+void PrintArr(int[] array, int first = 0)
+{
+    if(first != array.Length) 
+{
+        PrintArr(array, first + 1);
+        Console.Write($"{array[first]} ");
+    }
+}
+        
+void RecMas(string[] args)
+{
+    var array = new int[]{11, 22, 34, 4, 5 };
+    PrintArr(array);            
+}
+RecMas(args);
